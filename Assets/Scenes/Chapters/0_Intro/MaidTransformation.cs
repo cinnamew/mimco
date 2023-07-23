9f2e23ec5e1318bca1600aa234e03dc2fb4e7ea2
cinnamew <jolie.han@gmail.com>
2023-07-23 12:54:50 -0700

    maid transofrmaiotn and some of the ending

---
+++ Assets/Scenes/Chapters/0_Intro/MaidTransformation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class MaidTransformation : MonoBehaviour
+{
+    [SerializeField] int numPic;
+    [SerializeField] GameObject nextImage;
+    private bool m = false;
+    [SerializeField] Flowchart f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("godobye");
+        StartCoroutine(WaitAAH(1.0f));
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    private IEnumerator WaitAAH(float waitTime) {
+        //print("why hello");
+        yield return new WaitForSeconds(waitTime);
+        //print("done waiting");
+        m = true;
+    }
+
+    public void OnClick() {
+        print("hi " + m);
+        if(m == false) return;
+        if(numPic == 3) {
+            GetComponent<FadeToBlack>().antiFade();
+            WaitAAH(1.0f);
+            nextImage.SetActive(false);
+            f.ExecuteBlock("after dress");
+            return;
+        }
+        nextImage.SetActive(true);
+        GetComponent<FadeToBlack>().antiFade();
+    }
+}
