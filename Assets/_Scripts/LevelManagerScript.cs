ca5e7d7425e2e852c7d0a86f338bd9b515e87ce0
spiderface721 <borys2323@onet.pl>
2023-07-23 22:53:58 +0200

    stuff

--- Assets/_Scripts/LevelManagerScript.cs
+++ Assets/_Scripts/LevelManagerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class LevelManagerScript : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class LevelManagerScript : MonoBehaviour
     [SerializeField] int thingsFound;
 
     [SerializeField] TextMeshProUGUI text;
+
+    [SerializeField] int level;
 
     // Start is called before the first frame update
     void Start()
